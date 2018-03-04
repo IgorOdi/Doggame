9951241ccaee6a462d4c40a5e1915c31fb08ff00
Alexandre Magno <32045914+AlexandreMTH@users.noreply.github.com>
2018-03-04 19:04:22 -0300

    Gerador de Corredores + Pastas organizadas

---
+++ Dogger/Assets/_Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float walkSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+
+			//Anda pra Direita
+			if (Input.mousePosition.x >= Screen.width/2) {
+				transform.position += Vector3.left * Time.deltaTime * walkSpeed;
+			}
+
+			//Anda pra Esquerda
+			else {
+				transform.position += Vector3.right * Time.deltaTime * walkSpeed;
+			}
+		}
+	}
+}
---
+++ Dogger/Assets/_Scripts/Teste.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Teste : MonoBehaviour {
+
+	public Sprite[] walls;
+	public int numberOfWalls = 1;
+	public GameObject wallPrefab;
+
+
+	int lastImage;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < numberOfWalls; i++) {
+			GameObject wall = Instantiate (wallPrefab, transform, false);
+			wall.transform.localPosition = Vector3.right * i * 1080;
+			wall.name = "Wall_" + i;
+
+			int spriteIndex = Random.Range (0, walls.Length);
+
+			if (spriteIndex == lastImage) {
+				if (spriteIndex <= 0)
+					spriteIndex++;
+				else
+					spriteIndex--;
+			}
+
+			wall.GetComponent <Image> ().sprite = walls [spriteIndex];
+
+			lastImage = spriteIndex;
+		}
+			
+	}
+
+}
