aa0b1ce1b5ba2b9e916f9bfab56aa34f2535d919
Alexandre Magno <32045914+AlexandreMTH@users.noreply.github.com>
2018-03-04 21:28:00 -0300

    Anims + Lógica de Movimentação com min/max + Portas

--- Dogger/Assets/_Scripts/Movement.cs
+++ Dogger/Assets/_Scripts/Movement.cs
@@ -5,24 +5,57 @@ using UnityEngine;
 public class Movement : MonoBehaviour {
 
 	public float walkSpeed = 1;
+	public Transform heroes;
+
+	Animator[] an;
+	Transform cameraT;
+
+	float camMax;
+	float camMin;
+
+	bool reachedMaxPos;
+	bool reachedMinPos;
 
 	// Use this for initialization
 	void Start () {
-		
+		an = heroes.GetComponentsInChildren<Animator>();
+		cameraT = Camera.main.transform;
+		camMax = (((GameObject.Find ("Walls").GetComponent<Teste> ().numberOfWalls + 1) * 1080) - 432) * -1;
+		camMin = -432;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButton (0)) {
 
+		if  (gameObject.GetComponent<RectTransform>().localPosition.x <= camMax) {
+			reachedMaxPos = true;
+		} else if (gameObject.GetComponent<RectTransform>().localPosition.x >= camMin) {
+			reachedMinPos = true;
+		} else {
+			reachedMaxPos = false;
+			reachedMinPos = false;
+		}
+
+		//Se estiver clicando para andar
+		if (Input.GetMouseButton (0)) {
 			//Anda pra Direita
 			if (Input.mousePosition.x >= Screen.width/2) {
-				transform.position += Vector3.left * Time.deltaTime * walkSpeed;
+				if (!reachedMaxPos) transform.position += Vector3.left * Time.deltaTime * walkSpeed;
 			}
-
 			//Anda pra Esquerda
 			else {
-				transform.position += Vector3.right * Time.deltaTime * walkSpeed;
+				if (!reachedMinPos) transform.position += Vector3.right * Time.deltaTime * walkSpeed;
+			}
+
+
+			for (int i = 0; i < an.Length; i++) {
+				an [i].SetBool ("Walking", true);
+			}
+		}
+
+		else {
+			for (int i = 0; i < an.Length; i++) {
+				an [i].SetBool ("Walking", false);
 			}
 		}
 	}
--- Dogger/Assets/_Scripts/Teste.cs
+++ Dogger/Assets/_Scripts/Teste.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Teste : MonoBehaviour {
 
 	public Sprite[] walls;
+	public Sprite door;
 	public int numberOfWalls = 1;
 	public GameObject wallPrefab;
 
@@ -15,7 +16,7 @@ public class Teste : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-		for (int i = 0; i < numberOfWalls; i++) {
+		for (int i = 1; i < numberOfWalls + 1; i++) {
 			GameObject wall = Instantiate (wallPrefab, transform, false);
 			wall.transform.localPosition = Vector3.right * i * 1080;
 			wall.name = "Wall_" + i;
@@ -33,7 +34,16 @@ public class Teste : MonoBehaviour {
 
 			lastImage = spriteIndex;
 		}
-			
+
+		GameObject startDoor = Instantiate (wallPrefab, transform, false);
+		startDoor.transform.localPosition = Vector3.zero;
+		startDoor.name = "Start_Door";
+		startDoor.GetComponent<Image>().sprite = door;
+
+		GameObject endDoor = Instantiate (wallPrefab, transform, false);
+		endDoor.transform.localPosition = Vector3.right * (numberOfWalls + 1) * 1080;
+		endDoor.name = "End_Door";
+		endDoor.GetComponent<Image>().sprite = door;
 	}
 
 }
